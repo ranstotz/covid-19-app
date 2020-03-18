052a70d40b7f9c8da57ca00ef373bcd4dc0611b8
ranstotz <ryananstotz@gmail.com>
2020-03-17 21:08:54 -0400

    add build

---
+++ client/build/precache-manifest.5009854b25413f8587e3f1f45f316191.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7123d8c19a90dd4a29ddb3c70cb0cab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "097ffbe77c8f92478b6b",
+    "url": "/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "7a26d818f4ffa9b2fcbc",
+    "url": "/static/js/2.fa759ded.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.fa759ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "097ffbe77c8f92478b6b",
+    "url": "/static/js/main.3cbe1100.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- server/app.py
+++ server/app.py
@@ -2,19 +2,16 @@ from flask import Flask, Blueprint, request, render_template
 from config.index import Config
 from app import users
 
-app = Flask(__name__, static_folder-'../client/build')
-
 ##
 # View route
 ##
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def index(path):
-  '''Return index.html for all non-api routes'''
-  #pylint: disable=unused-argument
-  return send_from_directory(app.static_folder, 'index.html')
+def create_app(config_object=Config):
+    """ Factory function to start application  """
+    app = Flask(__name__.split('.')[0], static_folder='../client/build/static', template_folder="../client/build")
 
+    app.url_map.strict_slashes = False
 
+    return app
 
 
